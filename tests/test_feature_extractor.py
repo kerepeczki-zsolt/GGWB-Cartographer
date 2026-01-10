996602614dcb64a180f39cce4bb7a18a97b8a538
kerepeczki-zsolt <hankonoemi1991@gmail.com>
2026-01-10 18:21:07 +0100

    Update test_feature_extractor.py

--- tests/test_feature_extractor.py
+++ tests/test_feature_extractor.py
@@ -1,13 +1,11 @@
 import numpy as np
-from feature_extraction.geometric_features import FeatureExtractor
+from feature_extraction.geometric_features import GeometricFeatureExtractor
 
 def test_feature_extractor_output_shape():
-    extractor = FeatureExtractor()
+    extractor = GeometricFeatureExtractor()
     spec = np.random.rand(64, 64)
-    freqs = np.linspace(0, 1, 64)
-    times = np.linspace(0, 1, 64)
 
-    result = extractor.extract_all_features(spec, freqs, times)
+    result = extractor.extract_all_features(spec)
 
     assert isinstance(result, np.ndarray)
     assert result.shape == (92,)
